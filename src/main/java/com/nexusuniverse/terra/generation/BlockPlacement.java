614491e86a9ce74be27800d4a74f6ddcfc35b626
mrlion101az-cell <mrlion101az@gmail.com>
2026-07-29 07:55:12 -0700

    Delete src/main directory

--- src/main/java/com/nexusuniverse/terra/generation/BlockPlacement.java
+++
@@ -1,7 +0,0 @@
-package com.nexusuniverse.terra.generation;
-
-import org.bukkit.Material;
-
-/** One block to be placed at a specific position, relative to the world's origin. */
-public record BlockPlacement(int x, int y, int z, Material material) {
-}
