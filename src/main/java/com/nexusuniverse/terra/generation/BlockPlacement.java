21364eae0c4ace4c8e07bf808dc31624675f4ede
mrlion101az-cell <mrlion101az@gmail.com>
2026-07-30 21:02:13 -0700

    Delete src/main directory

--- src/main/java/com/nexusuniverse/terra/generation/BlockPlacement.java
+++
@@ -1,9 +0,0 @@
-package com.nexusuniverse.terra.generation;
-
-import org.bukkit.Material;
-
-public record BlockPlacement(int x, int y, int z, Material material, String label) {
-    public BlockPlacement(int x, int y, int z, Material material) {
-        this(x, y, z, material, null);
-    }
-}
